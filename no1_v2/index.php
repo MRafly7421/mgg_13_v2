dcce94b9af689bb532e1bc08587ef71ecfeade33
MRafly7421 <76813147+MRafly7421@users.noreply.github.com>
2021-11-22 14:14:24 +0700

    Update index.php

--- no1_v2/index.php
+++ no1_v2/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fungsi Mencari Hari dalam Seminggu</title>
-    <link rel="stylesheet" type="text/css" href="css/fday.css">
+    <link rel="stylesheet" type="text/css" href="css/fdayy.css">
 </head>
 <body>
     <div class="judul">
